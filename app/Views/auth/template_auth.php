54775d936b2b168de7b236dfae12f9b354534068
Saronggos <73785040+saronggos@users.noreply.github.com>
2021-06-24 23:21:40 +0700

    UP TO GITHUB

--- app/Views/auth/template_auth.php
+++ app/Views/auth/template_auth.php
@@ -8,7 +8,9 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
   <!-- Template CSS -->
   <link rel="stylesheet" href="<?=base_url()?>/assets/css/style.css">
   <link rel="stylesheet" href="<?=base_url()?>/assets/css/components.css">
